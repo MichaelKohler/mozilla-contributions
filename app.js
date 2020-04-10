db9489558dcad8e40cbbd814c195aa56778f43ec
Michael Kohler <me@michaelkohler.info>
2020-04-10 17:21:23 +0200

    Add express and template for contributions

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const StorageHandler = require('./lib/storage-handler');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', async (req, res) => {
+  const storageInstance = StorageHandler.getInstance();
+  const contributions = await storageInstance.getContributions();
+
+  res.render('index', { contributions });
+});
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((error, req, res) => {
+  res.status(error.status || 500); // eslint-disable-line no-magic-numbers
+  res.send('error');
+});
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 'use strict';
 
+const debug = require('debug')('mozilla-contributions:index');
 const gather = require('./lib/gather');
 const storageHandler = require('./lib/storage-handler');
+
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || '3333';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+server.on('listening', () => {
+  const addr = server.address();
+  debug(`Listening on ${addr.port}`);
+});
 
 (async function() {
   const storageInstance = storageHandler.getInstance();
